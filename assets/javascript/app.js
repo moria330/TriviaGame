302a10d6e5f1b4b04a80ef9e04c8cce524de8e2a
Matthew Jakobovic <mpjakobovic@gmail.com>
2018-06-08 23:41:12 -0400

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,32 +9,49 @@
 
 var correct = 0;
 var incorrect = 0;
-var flags = ["../images/Germany.svg","../images/Ivory_Coast.svg","../images/New_Zealand.svg","../images/Greece.svg","../images/Myanmar.svg","../images/Norway.svg","../images/Romania.svg","../images/Sweden.svg","../images/Malaysia.svg","../images/Ireland.svg"];
-var countries = ["Germany","Ivory Coast","New Zealand","Greece","Myanmar","Norway","Romania","Sweden","Malaysia","Ireland"];
-var answers = [];
-
-$(document).ready(function(){
-
-for (var i = 0; i < flags.length; i++) {
-	$("#question").html('<img src="' + flags[i] + '" class= "flags">');
-
-}
-
-if(answers[0] == countries[0])
+var questions = ["For extreamly large values of 2. Does 2 + 2 = 5 ?", "3^2 + 4^2 = 5^2?", "In Javascript does 3 + '3' = 33?", "log(10) = 1", "e"];
+var answers = [true, true, true, true, true];
+var userChoice = [];
 
 
-setTimeout(timeUp, 20000);
 
-function timeUp(){
-	alert("Time Up!");
-	alert("You answered " + correct + " correct. You answered " + incorrect + " incorrect.");
-}
+$(document).ready(function() {
+    for (var i = 0; i < questions.length; i++) {
+        var trivia = $(".trivia .card");
+        var cardHead = $("<div>");
+        cardHead.addClass("card-header");
+        cardHead.attr("id", "q" + i);
+        cardHead.text(questions[i]);
+        var cardBody = $("<div>");
+        cardBody.addClass("card-body");
+        cardBody.attr("id", "a" + i);
+        cardBody.text("lorem ipsum");
+        cardHead.append(cardBody);
+        trivia.append(cardHead);
 
-function correct() {
-	correct++;
-}
+    }
 
-function incorrect(){
-	incorrect++;
-}
+
+    //setTimeout(timeUp, 20000);
+
+    function timeUp() {
+        alert("Time Up!");
+        for (var i = 0; i < answers.length; i++) {
+            if (answers[i] == userChoice[i]) {
+                corr();
+            } else {
+                incorr();
+            }
+
+        }
+        alert("You answered " + correct + " correct. You answered " + incorrect + " incorrect.");
+    }
+
+    function corr() {
+        correct++;
+    }
+
+    function incorr() {
+        incorrect++;
+    }
 });
